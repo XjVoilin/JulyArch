ca4a1494575698c44ac1b0feee245617a1ff66df
xiaojia <1039996872@qq.com>
2026-05-18 15:19:17 +0800

    添加判空防御

--- Runtime/Core/Architecture/ArchExtensions.cs
+++ Runtime/Core/Architecture/ArchExtensions.cs
@@ -9,30 +9,30 @@ namespace JulyArch
         // ── ICanGetStore ──
 
         public static T GetStore<T>(this ICanGetStore self) where T : StoreBase
-            => self.GetArchitecture().GetStore<T>();
+            => self.GetArchitecture()?.GetStore<T>();
 
         // ── ICanEvent ──
 
         public static void Subscribe<T>(this ICanEvent self, Action<T> handler)
-            => self.GetArchitecture().Event.Subscribe(handler, self);
+            => self.GetArchitecture()?.Event?.Subscribe(handler, self);
 
         public static void Unsubscribe<T>(this ICanEvent self, Action<T> handler)
-            => self.GetArchitecture().Event.Unsubscribe(handler);
+            => self.GetArchitecture()?.Event?.Unsubscribe(handler);
 
         public static void UnsubscribeAll(this ICanEvent self)
-            => self.GetArchitecture().Event.UnsubscribeAll(self);
+            => self.GetArchitecture()?.Event?.UnsubscribeAll(self);
 
         public static void Publish<T>(this ICanEvent self, T eventData)
-            => self.GetArchitecture().Event.Publish(eventData);
+            => self.GetArchitecture()?.Event?.Publish(eventData);
 
         // ── ICanGetSystem ──
 
         public static T GetSystem<T>(this ICanGetSystem self) where T : GameSystemBase
-            => self.GetArchitecture().GetSystem<T>();
+            => self.GetArchitecture()?.GetSystem<T>();
 
         // ── ICanRunProcedure ──
 
         public static UniTask RunProcedure(this ICanRunProcedure self, ProcedureBase procedure, CancellationToken ct = default)
-            => self.GetArchitecture().RunProcedure(procedure, ct);
+            => self.GetArchitecture()?.RunProcedure(procedure, ct) ?? UniTask.CompletedTask;
     }
 }
